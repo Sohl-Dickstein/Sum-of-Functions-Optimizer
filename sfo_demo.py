1e5555c623c97a8e550b64fdba968bc3124695fd
Jascha Sohl-Dickstein <jascha.sohldickstein@gmail.com>
2014-05-05 15:30:35 -0700

    python demo to match matlab demo

---
+++ sfo_demo.py
@@ -0,0 +1,61 @@
+"""
+Train an autoencoder using SFO.
+
+Demonstrates usage of the Sum of Functions Optimizer (SFO) Python
+package.  See sfo.py and
+https://github.com/Sohl-Dickstein/Sum-of-Functions-Optimizer
+for additional documentation.
+
+Author: Jascha Sohl-Dickstein (2014)
+This software is made available under the Creative Commons
+Attribution-Noncommercial License.
+( http://creativecommons.org/licenses/by-nc/3.0/ )
+"""
+
+from numpy import *
+from numpy.random import randn
+from sfo import SFO
+
+# define an objective function and gradient
+def f_df(theta, v):
+    """
+    Calculate reconstruction error and gradient for an autoencoder with sigmoid
+    nonlinearity.
+    v contains the training data, and will be different for each subfunction.
+    """
+    h = 1./(1. + exp(-(dot(theta['W'], v) + theta['b_h'])))
+    v_hat = dot(theta['W'].T, h) + theta['b_v']
+    f = sum((v_hat - v)**2) / v.shape[1]
+    dv_hat = 2.*(v_hat - v) / v.shape[1]
+    db_v = sum(dv_hat, axis=1).reshape((-1,1))
+    dW = dot(h, dv_hat.T)
+    dh = dot(theta['W'], dv_hat)
+    db_h = sum(dh*h*(1.-h), axis=1).reshape((-1,1))
+    dW += dot(dh*h*(1.-h), v.T)
+    dfdtheta = {'W':dW, 'b_h':db_h, 'b_v':db_v}
+    return f, dfdtheta
+
+# set model and training data parameters
+M = 20 # number visible units
+J = 10 # number hidden units
+D = 100000 # full data batch size
+N = int(sqrt(D)/10.) # number minibatches
+# generate random training data
+v = randn(M,D)
+
+# create the array of subfunction specific arguments
+sub_refs = []
+for i in range(N):
+    # extract a single minibatch of training data.
+    sub_refs.append(v[:,i::N])
+
+# initialize parameters
+theta_init = {'W':randn(J,M), 'b_h':randn(J,1), 'b_v':randn(M,1)}
+# initialize the optimizer
+optimizer = SFO(f_df, theta_init, sub_refs)
+# # uncomment the following line to test the gradient of f_df
+# optimizer.check_grad()
+# run the optimizer for 1 pass through the data
+theta = optimizer.optimize(num_passes=1)
+# continue running the optimizer for another 50 passes through the data
+theta = optimizer.optimize(num_passes=50)
