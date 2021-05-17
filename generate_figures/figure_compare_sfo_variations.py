03ecf9a6c5532d87ba1df2783860a19137553def
Jascha Sohl-Dickstein <jaschasd@google.com>
2021-05-17 16:49:38 -0700

    Update figure_compare_sfo_variations.py

--- generate_figures/figure_compare_sfo_variations.py
+++ generate_figures/figure_compare_sfo_variations.py
@@ -6,11 +6,16 @@ Adjust the arrays "models_to_train" and "N_set" in the function
 "generate_data_SFO_variations()" in order to choose which objective functions
 are tested.
 
-Author: Jascha Sohl-Dickstein (2014)
-Web: http://redwood.berkeley.edu/jascha
-This software is made available under the Creative Commons
-Attribution-Noncommercial License.
-( http://creativecommons.org/licenses/by-nc/3.0/ )
+Copyright 2014 Jascha Sohl-Dickstein
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 """
 
 import convergence_utils
